b10e253b4b07bcb56a34940226ac2fe331cede56
Andrew Kelley <andrew@ziglang.org>
2024-01-08 18:21:45 -0700

    add new test: nanovg-example

--- build.zig
+++ build.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     add(b, "transitive_lib_headers_bug");
     add(b, "groovebasin");
+    add(b, "nanovg_example");
 }
 
 fn add(b: *std.Build, name: []const u8) void {
